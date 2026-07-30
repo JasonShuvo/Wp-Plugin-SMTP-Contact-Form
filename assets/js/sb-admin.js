130ecc9c99d84cfb1d9f67f0d8aae39ac29f5ec8
JasonShuvo <85076544+JasonShuvo@users.noreply.github.com>
2026-07-30 20:36:36 +0600

    Update sb-admin.js

--- assets/js/sb-admin.js
+++ assets/js/sb-admin.js
@@ -1,1 +1,26 @@
+jQuery(function ($) {
+	'use strict';
+
+	if ($.fn.wpColorPicker) {
+		$('.sbca-color-picker').wpColorPicker();
+	}
+
+	$('.sbca-media-btn').on('click', function (e) {
+		e.preventDefault();
+		var button = $(this);
+		var targetId = button.data('target');
+		var frame = wp.media({
+			title: 'Select Background Image',
+			multiple: false,
+			library: { type: 'image' },
+		});
+
+		frame.on('select', function () {
+			var attachment = frame.state().get('selection').first().toJSON();
+			$('#' + targetId).val(attachment.url);
+		});
+
+		frame.open();
+	});
+});
 
